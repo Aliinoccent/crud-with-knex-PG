980e4847fe92b9320e1ec1f690825736f58eaff9
unknown <aliinoccent786@gmail.com>
2025-06-25 04:49:49 -0700

    add expire 7d

--- utility/jwt.js
+++ utility/jwt.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 require("dotenv").config();
 exports.generateToken=(email)=>{
     try {
-        const token =jwt.sign(email,process.env.KEY);
+        const token =jwt.sign({email},process.env.KEY,{expiresIn:'7d'});
         return token ;
     } catch (error) {
         return error
